b4929ad51d7e89a16420bcab28e57af15360b64a
Audric POUZELGUES <a.pouzelgues@gmail.com>
2020-03-06 13:50:18 +0100

    Modification de la méthode de tirer et rechercheZone

--- src/miage/bataille/Partie.java
+++ src/miage/bataille/Partie.java
@@ -5,6 +5,7 @@ import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
 @objid ("df73f8f0-8de5-486c-802d-307ecfe68b1b")
 public class Partie {
+	
     /**
      * <Enter note text here>
      */
@@ -18,9 +19,20 @@ public class Partie {
     public ArrayList<Cellule> coups = new ArrayList<Cellule> ();
 
     @objid ("56659bde-06ae-44a1-abe1-82902d3b39cc")
-    public boolean tirer(int x, int y) {
-        return false;
+    public void tirer(int x, int y) {
+        Cellule celluleTiree; // Cellule tirée
+
+        // Vérifie si la cellule choisie à touché un bateau ou non
+    	if ((celluleTiree = rechercheZone(x, y)) == null) {
+    		
+    		celluleTiree = new Cellule(x, y);
+    		
+    	} else {
+    		
+    		celluleTiree.aEteTouche();
+    	}
     }
+    
 
     /**
      * Renvoie la Cellule recherchee, sinon renvois null
@@ -31,22 +43,26 @@ public class Partie {
     @objid ("2ef7390a-0dbf-41c9-b8d0-5f83c1742a80")
     private Cellule rechercheZone(int x, int y) {
         
-        Cellule trouve; // Cellule trouve dans une zone contigue
+        Cellule trouve = null; // Cellule trouve dans une zone contigue
         
         /* Parcours de toutes les zones de la mer */
-        for(ZoneContigue zone: compose) {
-            zone.existe(x, y);
+        for(int indiceRecherche = 0; indiceRecherche < compose.size(); indiceRecherche++) {
+            if( compose.get(indiceRecherche).existe(x, y)) {
+            	trouve = compose.get(indiceRecherche).getCellule(x, y);
+            }
         }
-        
-        /* Mer */
-        return null;
+
+        return trouve;
     }
 
     /**
      * Enregistre le coup pour la posterite
+     * Ajoute la cellule tirée à la liste des coups
+     * @param celulle tirée
      */
     @objid ("ce21b39e-cd8e-47d8-89ae-fe405c9c39ae")
     private void enregistrerCoup(Cellule celluleTiree) {
+    	
     }
   
     /**
