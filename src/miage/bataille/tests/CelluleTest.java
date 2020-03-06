b3bd762f7e132a3f02083b363a8cf10bc8090afe
Damien Avetta-Raymond <damien.avetta@gmail.com>
2020-03-06 12:14:43 +0100

    Création de tests pour Cellule.java

---
+++ src/miage/bataille/tests/CelluleTest.java
@@ -0,0 +1,37 @@
+/*
+ * Test de Cellule.java
+ * CelluleTest.java														06 mars 2020
+ * L3 MIAGE 2019/2020, ni copyright, ni copyleft
+ */
+package miage.bataille.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import miage.bataille.Cellule;
+
+/**
+ * Test des méthodes de la class Cellule
+ * @author Damien Avetta-Raymond
+ */
+class CelluleTest {
+	
+	/**
+	 * Test method for {@link miage.bataille.Cellule#Cellule(int, int)}.
+	 */
+	@Test
+	void testCellule() {
+		//assert (new Cellule(-1, -1)) != null;
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link miage.bataille.Cellule#aEteTouche()}.
+	 */
+	@Test
+	void testAEteTouche() {
+		fail("Not yet implemented");
+	}
+
+}
