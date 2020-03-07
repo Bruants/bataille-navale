cf2a88dea15dc5266418f965df0bcaed506a3b1a
SannacKevin <kevin.sannac08@hotmail.fr>
2020-03-07 18:32:13 +0100

    Ajouts de tests unitaires pour equals et aEteTouche

--- src/miage/bataille/tests/CelluleTest.java
+++ src/miage/bataille/tests/CelluleTest.java
@@ -24,13 +24,34 @@ class CelluleTest {
 	void testCellule() {
 		assert (new Cellule(0, 0)) != null;
 	}
+	
+	/**
+	 * Test method for {@link miage.bataille.Cellule#equals(Object)}.
+	 */
+	@Test
+	void testEquals() {
+		// Valid
+		assertTrue(new Cellule(0, 0).equals(new Cellule(0,0)));
+		assertTrue(new Cellule(4, 3).equals(new Cellule(4,3)));
+		assertTrue(new Cellule(1, 45).equals(new Cellule(1,45)));
+		
+		// Invalid
+		assertFalse(new Cellule(0, 0).equals(new Cellule(-1,0)));
+		assertFalse(new Cellule(0, -1).equals(new Cellule(0,0)));
+		assertFalse(new Cellule(0, 0).equals(new Cellule(0,15)));
+		assertFalse(new Cellule(42, 9).equals(new Cellule(12,105)));
+
+	}
 
 	/**
 	 * Test method for {@link miage.bataille.Cellule#aEteTouche()}.
 	 */
 	@Test
 	void testAEteTouche() {
-		fail("Not yet implemented");
+		Cellule aTester = new Cellule(0,0);
+		assertFalse(aTester.getTouche());
+		aTester.aEteTouche();
+		assertTrue(aTester.getTouche());
 	}
 
 }
