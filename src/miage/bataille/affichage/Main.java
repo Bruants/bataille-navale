5d797a27658d0ef6fd0bfc5992584e6d4dc945a4
Damien Avetta-Raymond <damien.avetta@gmail.com>
2020-03-10 23:34:29 +0100

    Affichage + saisie avec un seul caractère

--- src/miage/bataille/affichage/Main.java
+++ src/miage/bataille/affichage/Main.java
@@ -3,89 +3,125 @@
  */
 package miage.bataille.affichage;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 import miage.bataille.Cellule;
 import miage.bataille.Partie;
 
 /**
- * @author audric.pouzelgues
+ * @author Audric POUZELGUES, Damien AVETTA-RAYMOND
  */
 public class Main {
 
-
-	private static String carte;
+	private static HashMap<String, String> map = new HashMap<String, String>();
 
 	private static Partie partie = new Partie();
-
-	public static void actualiserCarte() {
-
-		//TODO: Corriger la taille de la carte
-		int tailleLargeur = 10,
-				tailleHauteur = 10;
+	
+	//TODO: Remplacer les tailles
+	private static int tailleLargeur = 5;
+	
+	private static int tailleHauteur = 5;
+	
+	/**
+	 * Afficher la carte de la bataille navale
+	 */
+	public static void afficherCarte() {
 		
-		// Nombre de cases disponibles sur la carte
-		int nombreDeCases = tailleLargeur * tailleLargeur;
+		char lettre = 'A';
+		String cle = "";
 		
-		String caractere; // caractère définissant le statut d'une cellule sur la carte
-		
-		StringBuilder builderCarte = new StringBuilder();
-		
-		// Tableau contenant toutes les cellules 
-		String[] tableau = new String[nombreDeCases];
-		
-		// Placement des caractères à afficher dans le tableau
-		for (int i = 1; i <= nombreDeCases; i++) {
-			caractere = " - ";
-			int ligne = i/tailleLargeur;
-			int colonne = i%tailleLargeur;
-			for (Cellule cellule : partie.getCellulesTirees()) {
+		for (int i = 0 ; i < tailleLargeur ; i++, lettre++) {
+			for (int j = 1 ; j <= tailleHauteur ; j++) {
 				
-				caractere = cellule.getCoordX() == colonne && cellule.getCoordY() == ligne ? " x ": caractere;
+				cle = lettre + "" + j;
+				System.out.print(map.get(cle) + " ");
 			}
 			
-			if (i%tailleLargeur == 0) {
-				caractere += "\n";
-			}
-			
-			tableau[i-1] = caractere;
+			System.out.print("\n");
 		}
-		
-		for (String elt : tableau) {
-			builderCarte.append(elt);
-		}
-		
-		carte = builderCarte.toString();
-		
 	}
+
+	/**
+	 * Initialise la carte à afficher
+	 */
+	public static void creerCarte() {
+		// Nombre de cases disponibles sur la carte
+		int nombreDeCases;
+		char lettre;
+		// Hash contenant toutes les cellules 
+		String celluleMap;
+		
+		lettre = 'A';	
+		nombreDeCases = tailleLargeur * tailleLargeur;
+		
+		for (int i = 0 ; i < tailleLargeur ; i++, lettre++) {
+			for (int j = 1 ; j <= tailleHauteur ; j++) {
+				celluleMap = Character.toString(lettre) + j;
+				map.put(celluleMap, " - ");
+			}
+		}
+	}
+	
+	
+	/**
+	 * Saisie d'un tir sur la carte
+	 * - Demande une lettre pour la colonne
+	 * - Demande un nombre entier positif pour la ligne
+	 */
+	public static String saisieTir() {
+		
+		String coordonnee;
+		
+		Scanner entree = new Scanner(System.in);
+		int ligne = 0;
+		char colonne = 0;
+		
+		boolean valide;
+		
+		do {		
+			
+			System.out.print("Saisir une coordonnée : ");
+			coordonnee = entree.next() + entree.nextLine();
+			
+			// Vérification de la taille de la chaine 
+			valide = coordonnee.length() == 2 && coordonnee.charAt(0) >= 65 && coordonnee.charAt(0) < 65 + tailleLargeur
+					&& coordonnee.charAt(1) >= 49 && coordonnee.charAt(1) <= 49 + tailleHauteur;
+			
+		} while (!valide);
+		
+		return coordonnee;
+	}
+	
 
 	/**
 	 * @param args non utilisé
 	 */
 	public static void main(String[] args) {
-
-		int x = 0, y = 0;
-
-		actualiserCarte();
-
-		System.out.println(carte.toString());
-
-		System.out.println("Tirer : ");
-		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("Saisir x : ");
-		x = sc.nextInt();
+		String coordonnee;
 		
-		System.out.print("Saisir y : ");
-		y = sc.nextInt();
+		creerCarte();
+		afficherCarte();
+		coordonnee = saisieTir();
+
+//		System.out.println("Tirer : ");
+//		Scanner sc = new Scanner(System.in);
+//		
+//		System.out.print("Saisir la colonne : ");
+//		x = sc.next().charAt(0);
+//		
+//		System.out.print("Saisir la ligne : ");
+//		y = sc.nextInt();
+				
+		map.put(coordonnee, " x ");		
+		partie.tirer(coordonnee.charAt(0) - 65, coordonnee.charAt(1) - 1);
 		
-		partie.tirer(x+1, y);
-
-		actualiserCarte();
+		System.out.println(Integer.parseInt(Character.toString(coordonnee.charAt(1))));
 		
-		System.out.println(carte.toString());
+		afficherCarte();
 
-
+		//System.out.println(map.toString());
 		//  rien = -
 		//  tiré = x
 		// touché = o
