d6cb52f8e9757950766a6abc9f4df7852e9c69a1
Damien Avetta-Raymond <damien.avetta@gmail.com>
2020-03-13 15:24:40 +0100

    Placement des bâteaux

--- src/miage/bataille/Partie.java
+++ src/miage/bataille/Partie.java
@@ -51,6 +51,7 @@ public class Partie {
 
     	compose = new ArrayList<ZoneContigue>();
     	coups = new ArrayList<Cellule>();
+    	placementFlotteAuto(config.getFlotte());
     	nbBatiments = compose.size();
     }
     
