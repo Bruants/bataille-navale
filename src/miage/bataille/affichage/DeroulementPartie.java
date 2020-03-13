78f8ac384b71aebc6eff9adae1f21d09aa606494
Audric POUZELGUES <a.pouzelgues@gmail.com>
2020-03-13 23:43:47 +0100

    Ajout entête de méthodes à compléter pour le déroulement d'une partie

--- src/miage/bataille/affichage/DeroulementPartie.java
+++ src/miage/bataille/affichage/DeroulementPartie.java
@@ -6,6 +6,7 @@ package miage.bataille.affichage;
 import java.util.HashMap;
 import java.util.Scanner;
 
+import miage.bataille.Cellule;
 import miage.bataille.Configuration;
 import miage.bataille.Partie;
 
@@ -15,9 +16,13 @@ import miage.bataille.Partie;
 public class DeroulementPartie {
 
 
-	private final static String MESSAGE_ERREUR_DE_SAISIE = "Les coordonnées saisies sont incorrectes. \n\n"
-			+ "Veuillez saisir des coordonnées du type LETTRE + CHIFFRE : 'A1' par exemple\n"
-			+ "et faisant partit de la carte.\n";
+	private final static String MESSAGE_ERREUR_DE_SAISIE = "Les coordonnées "
+			+ "saisies sont incorrectes. Les coordonnées ne doivent pas "
+			+ "dépasser la surface délimitée par la carte.\n";
+	
+	private final static String MESSAGE_INFORMATION_POUR_SAISIR_COORDONNEES = 
+			"Veuillez saisir des coordonnées du type LETTRE + CHIFFRE : "
+			+ "'A1' par exemple. \n";
 
 	/** 
 	 * Liste contenant la situation des différentes cellules 
@@ -25,15 +30,14 @@ public class DeroulementPartie {
 	 * * : touché
 	 * o : plouf
 	 */
-	private static HashMap<String, String> carte = new HashMap<String, String>();
+	private static HashMap<String, String> carte 
+					= new HashMap<String, String>();
 
-	/**
-	 * TODO
-	 */
+	
 	private static Partie partie;
 
 	/** Largeur de la carte */
-	private static int tailleLongueur;
+	private static int tailleLargeur;
 
 	/** Hauteur de la carte */
 	private static int tailleHauteur;
@@ -42,15 +46,16 @@ public class DeroulementPartie {
 	 * Débute une partie : - initialisation des différentes zones sur la map
 	 *                     - placement des bâtiments par le joueur
 	 */
+	//TODO: Prendre en compte la récupération d'une partie existante
+	//TODO: Pouvoir choisir une configuration existante ou prendre celle par défaut
 	public static void initialisation() {
-		Configuration config;
+		Configuration configurationDeLaPartie;
 		System.out.println("Début du jeu\n");
 		partie = new Partie();
-		config = partie.getConfiguration();
-		tailleHauteur = config.getHauteurCarte();
-		tailleLongueur = config.getLongueurCarte();
+		configurationDeLaPartie = partie.getConfiguration();
+		tailleHauteur = configurationDeLaPartie.getHauteurCarte();
+		tailleLargeur = configurationDeLaPartie.getLongueurCarte();
 		creerCarte();
-		afficherCarte();
 	}
 
 	/**
@@ -62,7 +67,7 @@ public class DeroulementPartie {
 		String cle;
 
 		System.out.print("   ");
-		for (int i = 1 ; i <= tailleLongueur ; i++) {
+		for (int i = 1 ; i <= tailleLargeur ; i++) {
 			if (i < 10) {
 				System.out.print("  " + i + "  ");
 			} else {
@@ -70,7 +75,7 @@ public class DeroulementPartie {
 			}
 		}
 		
-		for (int i = 0 ; i < tailleLongueur ; i++, lettre++) {
+		for (int i = 0 ; i < tailleLargeur ; i++, lettre++) {
 			System.out.print("\n\n");
 			System.out.print(lettre + "  ");
 			for (int j = 1 ; j <= tailleHauteur ; j++) {
@@ -91,7 +96,7 @@ public class DeroulementPartie {
 		String celluleMap;
 
 		lettre = 'A';	
-		for (int i = 0 ; i < tailleLongueur ; i++, lettre++) {
+		for (int i = 0 ; i < tailleLargeur ; i++, lettre++) {
 			
 			for (int j = 1 ; j <= tailleHauteur ; j++) {
 				celluleMap = Character.toString(lettre) + j;
@@ -103,14 +108,18 @@ public class DeroulementPartie {
 	
 	/**
 	 * Vérifie la saisie de l'utilisateur pour tirer
-	 * @return TRUE  -> Si la coordonnée en abcisses est un nombre
-	 * 				    compris entre 0 et la largeur de la carte.
-	 * 				    Et si la coordonées en ordonnés est une lettre
-	 * 				    de l'alphabet.
-	 * 		   FALSE -> Si la coordonnée en abcisses n'est pas comprise
-	 * 					entre 0 et la largeur de la carte, la coordonnée
-	 * 					en abcisses n'est pas un nombre ou si la coordonnée
-	 * 				    en ordonnés n'est pas une lettre.
+	 * @return TRUE  -> Si : 
+	 * 					- la coordonnée en abcisses est un nombre compris entre
+	 * 					  0 et la largeur de la carte.
+	 * 				    - et que la coordonées en ordonnés est une lettre
+	 * 				      de l'alphabet.
+	 * 		   FALSE -> Si : 
+	 * 					- la coordonnée en abcisses n'est pas comprise
+	 * 					  entre 0 et la largeur de la carte
+	 * 					- la coordonnée en abcisses n'est pas un 
+	 * 					  nombre
+	 * 					- la coordonnée en ordonnés n'est pas
+	 * 					  une lettre.
 	 */
 	public static boolean verifierSaisie(String coordonnee) {
 		
@@ -119,9 +128,10 @@ public class DeroulementPartie {
 		// Vérification de la taille et du contenu de la chaine 
 		try {
 			
-			valide = coordonnee.charAt(0) >= 65 && coordonnee.charAt(0) < 65 + tailleHauteur
-					&& Integer.parseInt(coordonnee.substring(1)) > 0 
-					&& Integer.parseInt(coordonnee.substring(1)) <= tailleLongueur;
+			valide = coordonnee.charAt(0) >= 65 
+				&& coordonnee.charAt(0) < 65 + tailleHauteur
+				&& Integer.parseInt(coordonnee.substring(1)) > 0 
+				&& Integer.parseInt(coordonnee.substring(1)) <= tailleLargeur;
 					
 		} catch (NumberFormatException e) {
 			valide = false;
@@ -138,7 +148,7 @@ public class DeroulementPartie {
 	 */
 	public static String saisieTir() {
 		Scanner entree = new Scanner(System.in);
-		String coordonnee;
+		String coordonnee = "";
 
 		do {		
 
@@ -156,46 +166,69 @@ public class DeroulementPartie {
 
 		return coordonnee;
 	}
+	
+	
+	/**
+	 * Lance une partie avec une nouvelle configuration
+	 * et initialise l'affichage de la partie. 
+	 * Fait dérouler la partie en faisant jouer l'utilisateur.
+	 */
+	//TODO: à remplacer si cela ne convient pas...
+	public static void LancerUnePartie() {
+		
+		//TODO: Compléter le corps de cette méthode à l'aide
+		// de ce qui a été écrit dans le main
+		
+		
+	}
+	
+	
+	/**
+	 * Lance une partie sauvegardée et réalise l'affichage de celle-ci
+	 * à l'aide de sa liste de coups déjà tirés.
+	 * @param partieExistante -> sauvegarde de la partie qui est
+	 * 		  récupérée pour reprendre une partie.
+	 */
+	//TODO: à remplacer si cela ne convient pas...
+	public static void LancerUnePartie(Partie partieExistante) {
+		//TODO: Compléter le corps de cette méthode à l'aide
+		     /* de ce qui a été écrit dans le main et en le modifiant
+			    pour afficher la carte à l'aide des coups déjà tirés */
+	}
 
 
 	/**
+	 * TODO: Créer une méthode LancerPartie() -> qui va réaliser ce que 
+	 * fait le main actuellement. Initialiser une partie, faire dérouler la partie
+	 * et la fin de la partie
 	 * @param args non utilisé
 	 */
 	public static void main(String[] args) {
 		String coordonnee;
 		String resultat; // Le résultat d'un tir
 
-		int coordonneeX, coordonneeY;
+		int coordonneeX, 
+			coordonneeY;
 
 		/* Phase 1 : initialisation de la partie */
 		initialisation();
+		afficherCarte();
 		
 		/* Phase 2 : Déroulement d'un tour */
 		for (int nbTour = 0; partie.getNbBatiments() > -1; nbTour++) {
-			System.out.print("Coup " + nbTour + " > ");
+			System.out.print("Coup " + nbTour + " => ");
 			
 			// Récupération des coordonnées saisies
 			coordonnee = saisieTir();	
 			
 			// Conversion des coordonnées saisies pour pouvoir tirer
 			coordonneeX = Integer.parseInt(coordonnee.substring(1));
-			coordonneeX -= 1;
-			
+			coordonneeX -= 1;	
 			coordonneeY = coordonnee.charAt(0);
 			coordonneeY -= 65;
 			
 			// Tir sur la carte
 			resultat = partie.tirer(coordonneeX, coordonneeY);
-			
-//			int y = coordonnee.charAt(0);
-//			y -= 65;
-//			
-//			int x = Integer.parseInt(coordonnee.substring(1));
-//			x -= 1;
-//			System.out.println("X : " + x);
-//			System.out.println("Y : " + y);
-			//resultat = partie.tirer(Integer.parseInt(coordonnee.substring(1)),
-			//		Integer.parseInt(Character.toString(coordonnee.charAt(0))));
 			
 			if (resultat.equals("plouf")) {
 				carte.put(coordonnee, "  o  ");
@@ -204,6 +237,7 @@ public class DeroulementPartie {
 			}
 			System.out.println(resultat + " !");
 			afficherCarte();
+
 		}
 		/* Phase 3 : Fin de la partie */
 		//TODO
