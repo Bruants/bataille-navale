89f9652edcbb37f7ec5c285e707e22043070b76b
Audric POUZELGUES <a.pouzelgues@gmail.com>
2020-03-14 12:48:00 +0100

    Ajout de méthodes pour gérer les configs
    - Initialisation par défaut de l'affichage avec une config par défaut
    
    - Initialisation de l'affichage avec une configuration sélectionnée
    
    - Initialisation l'affichage avec une partie sauvegardee
    
    - Creer une carte vide avec ' - '
    
    - Creer une carte en prennant en compte la partie sauvegardée et les
    zones tirées et/ou touchées
    
    - Lancer une partie
    
    - Lancer une partie avec une partie sauvegardé
    
    - Les nouvelles méthodes n'ont pas étés testés...

--- src/miage/bataille/affichage/DeroulementPartie.java
+++ src/miage/bataille/affichage/DeroulementPartie.java
@@ -6,6 +6,8 @@ package miage.bataille.affichage;
 import java.util.HashMap;
 import java.util.Scanner;
 
+import org.hamcrest.core.IsNull;
+
 import miage.bataille.Cellule;
 import miage.bataille.Configuration;
 import miage.bataille.Partie;
@@ -19,10 +21,22 @@ public class DeroulementPartie {
 	private final static String MESSAGE_ERREUR_DE_SAISIE = "Les coordonnées "
 			+ "saisies sont incorrectes. Les coordonnées ne doivent pas "
 			+ "dépasser la surface délimitée par la carte.\n";
-	
+
 	private final static String MESSAGE_INFORMATION_POUR_SAISIR_COORDONNEES = 
 			"Veuillez saisir des coordonnées du type LETTRE + CHIFFRE : "
-			+ "'A1' par exemple. \n";
+					+ "'A1' par exemple. \n";
+
+	private final static String MESSAGE_ERREUR_SAISIE_INITIALISATION_CONFIGURATION = "La"
+			+ "configuration choisie ne peut pas être null.";
+
+
+	private static String coordonnee;
+
+	private static int coordonneeX;
+
+	private static int coordonneeY;
+
+	private static String resultat;
 
 	/** 
 	 * Liste contenant la situation des différentes cellules 
@@ -31,9 +45,9 @@ public class DeroulementPartie {
 	 * o : plouf
 	 */
 	private static HashMap<String, String> carte 
-					= new HashMap<String, String>();
+	= new HashMap<String, String>();
 
-	
+
 	private static Partie partie;
 
 	/** Largeur de la carte */
@@ -43,19 +57,57 @@ public class DeroulementPartie {
 	private static int tailleHauteur;
 
 	/**
-	 * Débute une partie : - initialisation des différentes zones sur la map
+	 * Débute une partie par défaut : 
+	 * 					   - initialisation des différentes zones sur la map
 	 *                     - placement des bâtiments par le joueur
 	 */
-	//TODO: Prendre en compte la récupération d'une partie existante
-	//TODO: Pouvoir choisir une configuration existante ou prendre celle par défaut
-	public static void initialisation() {
+	public static void initialisationParDefaut() {
 		Configuration configurationDeLaPartie;
 		System.out.println("Début du jeu\n");
 		partie = new Partie();
 		configurationDeLaPartie = partie.getConfiguration();
 		tailleHauteur = configurationDeLaPartie.getHauteurCarte();
 		tailleLargeur = configurationDeLaPartie.getLongueurCarte();
-		creerCarte();
+		creerNouvelleCarte();
+	}
+
+	/**
+	 * Débute une partie avec une configuration choisie
+	 * 					   - initialisation des différentes zones sur la map
+	 *                     - placement des bâtiments par le joueur
+	 * @param configurationChoisie
+	 */
+	public static void initialisationAvecUneConfiguration(Configuration configurationChoisie) {
+
+		if (configurationChoisie == null) {
+			initialisationParDefaut();
+		} else {
+			System.out.println("Début du jeu\n");
+			partie = new Partie(configurationChoisie);
+			tailleHauteur = configurationChoisie.getHauteurCarte();
+			tailleLargeur = configurationChoisie.getLongueurCarte();
+			creerNouvelleCarte();
+		}
+	}
+
+	/**
+	 * Initialise l'affichage de la partie avec un sauvegarde existante
+	 * @param partieSauvegardee
+	 */
+	public static void initialisationAvecUneSauvegarde(Partie partieSauvegardee) {
+
+		Configuration configurationDePartie;
+
+		if (partieSauvegardee == null) {
+			initialisationParDefaut();
+		} else {
+			System.out.println("Début du jeu\n");
+			partie = partieSauvegardee;
+			configurationDePartie = partie.getConfiguration();
+			tailleHauteur = configurationDePartie.getHauteurCarte();
+			tailleLargeur = configurationDePartie.getLongueurCarte();
+			creerCarteAPartirDeSauvegarde();
+		}
 	}
 
 	/**
@@ -67,14 +119,14 @@ public class DeroulementPartie {
 		String cle;
 
 		System.out.print("   ");
-		for (int i = 1 ; i <= tailleLargeur ; i++) {
+		for (int i = 1 ; i <= tailleHauteur ; i++) {
 			if (i < 10) {
 				System.out.print("  " + i + "  ");
 			} else {
 				System.out.print("  " + i + " ");
 			}
 		}
-		
+
 		for (int i = 0 ; i < tailleLargeur ; i++, lettre++) {
 			System.out.print("\n\n");
 			System.out.print(lettre + "  ");
@@ -83,29 +135,59 @@ public class DeroulementPartie {
 				System.out.print(carte.get(cle));
 			}		
 		}
-		
+
 		System.out.print("\n\n");
 	}
 
 	/**
-	 * Initialise la carte à afficher
+	 * Initialise la carte vide à afficher
 	 */
-	public static void creerCarte() {
+	public static void creerNouvelleCarte() {
 		char lettre;
 		// Hash contenant toutes les cellules 
 		String celluleMap;
 
 		lettre = 'A';	
 		for (int i = 0 ; i < tailleLargeur ; i++, lettre++) {
-			
+
 			for (int j = 1 ; j <= tailleHauteur ; j++) {
 				celluleMap = Character.toString(lettre) + j;
 				carte.put(celluleMap, "  -  ");
 			}
 		}
 	}
-	
-	
+
+	/**
+	 * Initialise la carte  à afficher à partir d'une sauvegarde existante
+	 */
+	//TODO: A tester
+	public static void creerCarteAPartirDeSauvegarde() {
+		char lettre;
+		// Hash contenant toutes les cellules 
+		String celluleMap;
+
+
+		for (Cellule c : partie.coups) {
+
+			celluleMap = Character.toString((char)(c.getCoordY() + 65)) + c.getCoordX();
+
+			if (partie.getCompose().contains(c)) {
+				carte.put(celluleMap, "  *  ");
+			} else {
+				carte.put(celluleMap, "  o  ");
+			}
+		}
+		//		lettre = 'A';	
+		//		for (int i = 0 ; i < tailleLargeur ; i++, lettre++) {
+		//
+		//			for (int j = 1 ; j <= tailleHauteur ; j++) {
+		//				celluleMap = Character.toString(lettre) + j;
+		//				carte.put(celluleMap, "  -  ");
+		//			}
+		//		}
+	}
+
+
 	/**
 	 * Vérifie la saisie de l'utilisateur pour tirer
 	 * @return TRUE  -> Si : 
@@ -122,21 +204,21 @@ public class DeroulementPartie {
 	 * 					  une lettre.
 	 */
 	public static boolean verifierSaisie(String coordonnee) {
-		
+
 		boolean valide = false; 
-		
+
 		// Vérification de la taille et du contenu de la chaine 
 		try {
-			
+
 			valide = coordonnee.charAt(0) >= 65 
-				&& coordonnee.charAt(0) < 65 + tailleHauteur
-				&& Integer.parseInt(coordonnee.substring(1)) > 0 
-				&& Integer.parseInt(coordonnee.substring(1)) <= tailleLargeur;
-					
+					&& coordonnee.charAt(0) < 65 + tailleHauteur
+					&& Integer.parseInt(coordonnee.substring(1)) > 0 
+					&& Integer.parseInt(coordonnee.substring(1)) <= tailleLargeur;
+
 		} catch (NumberFormatException e) {
 			valide = false;
 		}
-		
+
 		return valide;
 	}
 
@@ -154,9 +236,9 @@ public class DeroulementPartie {
 
 			System.out.print("Saisir une coordonnée : ");
 			coordonnee = entree.next() + entree.nextLine();
-			
+
 			coordonnee = coordonnee.toUpperCase();
-			
+
 			// Message d'erreur si les coordonnées sont non valides
 			if (!verifierSaisie(coordonnee)) {
 				System.out.println(MESSAGE_ERREUR_DE_SAISIE);
@@ -166,8 +248,8 @@ public class DeroulementPartie {
 
 		return coordonnee;
 	}
-	
-	
+
+
 	/**
 	 * Lance une partie avec une nouvelle configuration
 	 * et initialise l'affichage de la partie. 
@@ -175,14 +257,46 @@ public class DeroulementPartie {
 	 */
 	//TODO: à remplacer si cela ne convient pas...
 	public static void LancerUnePartie() {
-		
-		//TODO: Compléter le corps de cette méthode à l'aide
-		// de ce qui a été écrit dans le main
-		
-		
+
+		/* Phase 1 : initialisation de la partie */
+		Configuration config = new Configuration().recupererConfig("Config1");
+		initialisationAvecUneConfiguration(config);
+
+		afficherCarte();
+
+		/* Phase 2 : Déroulement d'un tour */
+		for (int nbTour = 0; partie.getNbBatiments() > -1; nbTour++) {
+			System.out.print("Coup " + nbTour + " => ");
+
+			// Récupération des coordonnées saisies
+			coordonnee = saisieTir();	
+
+			// Conversion des coordonnées saisies pour pouvoir tirer
+			coordonneeX = Integer.parseInt(coordonnee.substring(1));
+			coordonneeX -= 1;	
+			coordonneeY = coordonnee.charAt(0);
+			coordonneeY -= 65;
+
+			// Tir sur la carte
+			resultat = partie.tirer(coordonneeX, coordonneeY);
+
+			if (resultat.equals("plouf")) {
+				carte.put(coordonnee, "  o  ");
+			} else {
+				carte.put(coordonnee, "  *  ");
+			}
+			System.out.println(resultat + " !");
+			afficherCarte();
+
+		}
+
+
+
+
+
 	}
-	
-	
+
+
 	/**
 	 * Lance une partie sauvegardée et réalise l'affichage de celle-ci
 	 * à l'aide de sa liste de coups déjà tirés.
@@ -192,8 +306,39 @@ public class DeroulementPartie {
 	//TODO: à remplacer si cela ne convient pas...
 	public static void LancerUnePartie(Partie partieExistante) {
 		//TODO: Compléter le corps de cette méthode à l'aide
-		     /* de ce qui a été écrit dans le main et en le modifiant
+		/* de ce qui a été écrit dans le main et en le modifiant
 			    pour afficher la carte à l'aide des coups déjà tirés */
+
+		/* Phase 1 : initialisation de la partie */
+		initialisationAvecUneSauvegarde(partieExistante);
+		afficherCarte();
+
+		/* Phase 2 : Déroulement d'un tour */
+		for (int nbTour = 0; partie.getNbBatiments() > -1; nbTour++) {
+			System.out.print("Coup " + nbTour + " => ");
+
+			// Récupération des coordonnées saisies
+			coordonnee = saisieTir();	
+
+			// Conversion des coordonnées saisies pour pouvoir tirer
+			coordonneeX = Integer.parseInt(coordonnee.substring(1));
+			coordonneeX -= 1;	
+			coordonneeY = coordonnee.charAt(0);
+			coordonneeY -= 65;
+
+			// Tir sur la carte
+			resultat = partie.tirer(coordonneeX, coordonneeY);
+
+			if (resultat.equals("plouf")) {
+				carte.put(coordonnee, "  o  ");
+			} else {
+				carte.put(coordonnee, "  *  ");
+			}
+			System.out.println(resultat + " !");
+			afficherCarte();
+
+		}
+
 	}
 
 
@@ -204,41 +349,8 @@ public class DeroulementPartie {
 	 * @param args non utilisé
 	 */
 	public static void main(String[] args) {
-		String coordonnee;
-		String resultat; // Le résultat d'un tir
-
-		int coordonneeX, 
-			coordonneeY;
-
-		/* Phase 1 : initialisation de la partie */
-		initialisation();
-		afficherCarte();
 		
-		/* Phase 2 : Déroulement d'un tour */
-		for (int nbTour = 0; partie.getNbBatiments() > -1; nbTour++) {
-			System.out.print("Coup " + nbTour + " => ");
-			
-			// Récupération des coordonnées saisies
-			coordonnee = saisieTir();	
-			
-			// Conversion des coordonnées saisies pour pouvoir tirer
-			coordonneeX = Integer.parseInt(coordonnee.substring(1));
-			coordonneeX -= 1;	
-			coordonneeY = coordonnee.charAt(0);
-			coordonneeY -= 65;
-			
-			// Tir sur la carte
-			resultat = partie.tirer(coordonneeX, coordonneeY);
-			
-			if (resultat.equals("plouf")) {
-				carte.put(coordonnee, "  o  ");
-			} else {
-				carte.put(coordonnee, "  *  ");
-			}
-			System.out.println(resultat + " !");
-			afficherCarte();
-
-		}
+		LancerUnePartie();
 		/* Phase 3 : Fin de la partie */
 		//TODO
 	}
