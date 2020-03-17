44951e71e3483ed2e3f82e0c068bac9ae1e46778
Damien Avetta-Raymond <damien.avetta@gmail.com>
2020-03-17 22:34:16 +0100

    Création des tests déroulement partie (affichage)

---
+++ src/miage/bataille/tests/affichage/DeroulementPartieTest.java
@@ -0,0 +1,48 @@
+package miage.bataille.tests.affichage;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DeroulementPartieTest {
+
+	@Test
+	void testVerifierSaisie() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSaisieTour() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testQuitterPartie() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSaisieNom() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testNomFichierCorrect() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSauvegardePeutEtreCree() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testReponseValide() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRechercheFichier() {
+		fail("Not yet implemented");
+	}
+}
